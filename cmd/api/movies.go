9e898e71af6293b68fa3ec6c4cee8f664acd3034
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-04-26 16:58:26 +0500

    add validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"greenlight/internal/data"
+	"greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -19,6 +20,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.badRequest(w, r, err)
 		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 	}
 
 	fmt.Fprintf(w, "%+v\n", input)
---
+++ internal/validator/validator.go
@@ -0,0 +1,58 @@
+package validator
+
+import "regexp"
+
+// EmailRX email validation is taken from https://html.spec.whatwg.org/#valid-e-mail-address
+var (
+	EmailRX = regexp.MustCompile("/^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/")
+)
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func In(value string, list ...string) bool {
+	for i := range list {
+		if value == list[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func Unique(values []string) bool {
+	unique := make(map[string]struct{})
+
+	for _, value := range values {
+		unique[value] = struct{}{}
+	}
+
+	return len(unique) == len(values)
+}
